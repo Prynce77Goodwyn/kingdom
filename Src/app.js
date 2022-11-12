c3abf94da9d48faac24013bbdd28129384cc3f06
Sugey Banegas <sugeybanegas@Sugeys-Air.lan>
2022-11-12 15:00:38 -0600

    Version 0.. I need to make a responsive nav for  mobile

--- Src/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerbutton = document.getElementById(".hamburgerbutton");
-const heroNavMenu = document.getElementById(".heroNavMenu");
-
-hamburgerbutton.addEventListener("click", toggleButton)
-
-function toggleButton(){
-	heroNavMenu.classList.toggle("show");
-}
